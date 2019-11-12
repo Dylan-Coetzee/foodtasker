099951b71ffa81ea79d550f84c65381ba6fc14a7
Dylan <djaycoetzee@live.com>
2019-11-12 23:09:48 +0200

    Complete meals pages list/add/edit and side menu improvements

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -41,9 +41,11 @@ urlpatterns = [
         name = 'restaurant-sign-up'),
     url(r'^restaurant/account/$', views.restaurant_account, name='restaurant-account'),
     url(r'^restaurant/meal/$', views.restaurant_meal, name='restaurant-meal'),
+    url(r'^restaurant/meal/add/$', views.restaurant_add_meal, name='restaurant-add-meal'),
+    url(r'^restaurant/meal/edit/(?P<meal_id>\d+)/$', views.restaurant_edit_meal, name='restaurant-edit-meal'),
     url(r'^restaurant/order/$', views.restaurant_order, name='restaurant-order'),
     url(r'^restaurant/report/$', views.restaurant_report, name='restaurant-report'),
-    
+
     # Sign In/ Sign Up/ Sign Out
     url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
     # (r'^auth/', include('rest_framework_social_oauth2.urls')),
