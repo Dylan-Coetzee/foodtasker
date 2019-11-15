2c877c580d25880abb0fc19f983f60c319848849
Dylan <djaycoetzee@live.com>
2019-11-15 22:16:09 +0200

    Complete drive location API

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -51,6 +51,7 @@ urlpatterns = [
     url(r'^api/customer/meals/(?P<restaurant_id>\d+)/$', apis.customer_get_meals),
     url(r'^api/customer/order/add/$', apis.customer_add_order),
     url(r'^api/customer/order/latest/$', apis.customer_get_latest_order),
+    url(r'^api/customer/driver/location/$', apis.customer_driver_location),
     #Notification when a new order is raised
     url(r'^api/restaurant/order/notification/(?P<last_request_time>.+)/$', apis.restaurant_order_notification),
 
@@ -60,5 +61,6 @@ urlpatterns = [
     url(r'^api/driver/orders/latest/$', apis.driver_get_latest_order),
     url(r'^api/driver/orders/complete/$', apis.driver_complete_order),
     url(r'^api/driver/revenue/$', apis.driver_get_revenue),
+    url(r'^api/driver/location/update/$', apis.driver_update_location),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
