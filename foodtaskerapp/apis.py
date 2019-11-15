17a9ad36282c9d777e0eed2aab51b9ee0fa3d124
Dylan <djaycoetzee@live.com>
2019-11-15 16:32:00 +0200

    Complete drivers api

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -109,14 +109,91 @@ def driver_get_ready_orders(request):
 
     return JsonResponse({"orders": orders})
 
+@csrf_exempt
 def driver_pick_order(request):
+
+    if request.method == "POST":
+        #Get Token
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt = timezone.now())
+
+        #Get Driver
+        driver = access_token.user.driver
+
+        #Check if driver can only up one order at the same timezone
+        if Order.objects.filter(driver = driver).exclude(status = Order.ONTHEWAY):
+            return JsonResponse({"status": "failed", "error": "You can only pick order at the same time."})
+
+        try:
+            order = Order.objects.get(
+                id = request.POST["order_id"],
+                driver = None,
+                status = Order.READY
+            )
+            order.driver = driver
+            order.status = Order.ONTHEWAY
+            order.picked_at = timezone.now()
+            order.save()
+
+            return JsonResponse({"status": "success"})
+
+        except Order.DoesNotExist:
+            return JsonResponse({"status": "failed", "error": "This order has been picked up by another driver."})
+
     return JsonResponse({ })
 
+#Get Request
+#Params: access_token
 def driver_get_latest_order(request):
-    return JsonResponse({ })
+    #Get Token
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+        expires__gt = timezone.now())
 
+    #Get Driver
+    driver = access_token.user.driver
+    order = OrderSerializer(
+        Order.objects.filter(driver = driver).order_by("picked_at").last(),
+    ).data
+
+    return JsonResponse({ "order": order })
+
+#POST
+@csrf_exempt
 def driver_complete_order(request):
-    return JsonResponse({ })
+    access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+        expires__gt = timezone.now())
 
+    #Get Driver
+    driver = access_token.user.driver
+    order = Order.objects.get(id = request.POST["order_id"], driver = driver)
+    order.status = Order.DELIVERED
+    order.save()
+
+    return JsonResponse({"status": "success"})
+
+# Get params: access_token
 def driver_get_revenue(request):
-    return JsonResponse({ })
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+        expires__gt = timezone.now())
+
+    #Get Driver
+    driver = access_token.user.driver
+
+    from datetime import timedelta
+
+    revenue = {}
+    today = timezone.now()
+    current_weekdays = [today + timedelta(days = i) for i in range(0 - today.weekday(), 7 - today.weekday())]
+
+    for day in current_weekdays:
+        orders = Order.objects.filter(
+            driver = driver,
+            status = Order.DELIVERED,
+            created_at__year = day.year,
+            created_at__month = day.month,
+            created_at__day = day.day,
+        )
+
+        revenue[day.strftime("%a")] = sum(order.total for order in orders)
+
+    return JsonResponse({"revenue": revenue})
